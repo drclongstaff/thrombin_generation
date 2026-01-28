df51d5773c690a593d1e6459a0e5403dd4933846
drclongstaff <drclongstaff@gmail.com>
2026-01-28 15:34:32 +0000

    Make folder for derivative curve analysis

--- ui.R
+++ ui.R
@@ -12,6 +12,7 @@ library(vroom)
 source("./Functions/Session_info.R")
 source("./Functions/Load_file.R")
 source("./Functions/Polynomials.R")
+source("./Functions/Deriv_curve_analysis.R")
 
 Thisversion <- "1.001dx4"
 thematic::thematic_shiny()
